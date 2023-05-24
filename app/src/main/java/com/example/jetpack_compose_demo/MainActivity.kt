9a5727354c70bd738292ca84f53effc678cae2cd
badkem <hoangdinhthangloi@gmail.com>
2023-05-24 21:13:20 +0700

    test

--- app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
@@ -3,23 +3,35 @@ package com.example.jetpack_compose_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_demo.ui.theme.Jetpack_compose_demoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Jetpack_compose_demoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            MyApp {
+                val textState = remember { mutableStateOf(TextFieldValue()) }
+
+                Column(modifier = Modifier.padding(all = 8.dp)) {
+                    Greeting(textState.value.text, modifier = Modifier.padding(bottom = 8.dp))
+                    TextField(value = textState.value, onValueChange = { textState.value = it }, placeholder = { Text("Login") })
                 }
             }
         }
@@ -27,10 +39,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    Jetpack_compose_demoTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
